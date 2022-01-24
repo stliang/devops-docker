fd7071aab263bce71a3d09c37f7b26777755018b
Steve Liang <stliang@gmail.com>
2022-01-23 22:15:21 -0800

    Update docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 
 # Does:
-# 1.) Mounts your native local directory where you store your github repos
-# 2.) Mounts your native local gcloud config
-# 3.) Executes docker run with the mounts
+# 1.) Mount your native local directory where you store your github repos
+# 2.) Mount your native local gcloud config directory
+# 3.) Mount your native local kubectl config directory
+# 4.) Mount your native local zsh history file
+# 5.) Mount your native local .gitconfig file
+# 6.) Executes docker run with the above mounts
 
 # Assumptions
 # 1.) Public cloud provider is GCP
-# 2.) This devops-docker is built and published to a container registry such as GCR
+# 2.) This devops-docker is built and published to a container registry such as dockerhub or GCR
 # 3.) You specify the local directory of your stored your github repos in $GITHUB_DIR
+
+# Usage
+# The git, kubectl, and gcloud utilities are installed by Dockerfile.
+# Use these utilities to authentical with their respective providers.
+# Because their credential file or directories are mounted to your local file system,
+# the credential will be saved in your native local file system instead of the docker container.
 
 
 DOCKER_PLATFORM="${DOCKER_PLATFORM:-linux/amd64}"  # Default linux/amd64
